42c96312d9cd3c1b7343d34f06f1e740475da943
황지훈 <s24072@gsm.hs.kr>
2025-07-21 17:32:29 +0900

    init

---
+++ src/test/java/team/wwp/wisp/WispApplicationTests.java
@@ -0,0 +1,13 @@
+package team.wwp.wisp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WispApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
