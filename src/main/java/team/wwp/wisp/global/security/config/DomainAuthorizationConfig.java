1af8c3cbef51b1fc84520d55199a099b47e6c7e1
황지훈 <s24072@gsm.hs.kr>
2025-07-26 01:18:21 +0900

    feat: 도메인 권한 설정을 위한 DomainAuthorizationConfig 클래스 추가

---
+++ src/main/java/team/wwp/wisp/global/security/config/DomainAuthorizationConfig.java
@@ -0,0 +1,14 @@
+package team.wwp.wisp.global.security.config;
+
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.stereotype.Component;
+
+@Component
+public class DomainAuthorizationConfig {
+    
+    public void configureAuthorization(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests(auth -> auth
+            .anyRequest().permitAll()
+        );
+    }
+}
